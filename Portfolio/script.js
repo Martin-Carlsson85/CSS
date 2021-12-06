8400059c2ef9f640fcf242c025c8686ba11f9242
Martin-Carlsson85 <martin@webfixit.se>
2021-12-06 09:34:46 +0100

    Starting Omnifood project

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,21 +1,26 @@
+//Mouse circle
 const mouseCirlce = document.querySelector(".mouse-circle");
 const mouseDot = document.querySelector(".mouse-dot");
 
-//Mouse Circle
 const mouseCirlceFn = (x, y) => {
   mouseCirlce.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`;
   mouseDot.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`;
 };
+//End of mouse circle
+
+//Animated circles
+
+//End of animated circles
 
 document.body.addEventListener("mousemove", (e) => {
   let x = e.clientX;
   let y = e.clientY;
 
   mouseCirlceFn(x, y);
+  animateCircles(e, x, y);
 });
 
 document.body.addEventListener("mouseleave", () => {
   mouseCirlce.style.opacity = "0";
   mouseDot.style.opacity = "0";
 });
-//End of Mouse Circle
