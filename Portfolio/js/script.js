14134c609d85222b1295cd32c6baa1ba4db37cc3
Martin-Carlsson85 <martin@webfixit.se>
2022-01-24 22:04:18 +0100

    Adding title section

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -4,4 +4,11 @@ $(document).ready(function () {
     play: 5000,
     pagination: false,
   });
+  var typed = new Typed(".typed", {
+    strings: ["Software Engineer.", "Web Developer", "Junior."],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false,
+  });
 });
